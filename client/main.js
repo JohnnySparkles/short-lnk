361b27e32f4b91ca64d69e084d4f5f196322e96f
John Wieland <johnny.c.sparkles@gmail.com>
2018-07-04 13:29:03 +1000

    Added Link, Login, NotFound, and Signup routes.

--- client/main.js
+++ client/main.js
@@ -1,13 +1,34 @@
+
+// Package Imports
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Application Imports
 import './main.html';
 import Signup from './../imports/ui/Signup';
+import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+import Login from './../imports/ui/Login';
+
+
+const routes = (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/" component={Login}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/links" component={Link}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+  </Router>
+);
 
 
 Meteor.startup(() => {
-  ReactDOM.render(<Signup/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
-
 
 
