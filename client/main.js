5d6b6e3a67b68e9ed2747b3a82718572357af18d
John Wieland <johnny.c.sparkles@gmail.com>
2018-07-04 12:58:02 +1000

    Initial commit of short-lnk

---
+++ client/main.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.html';
+
+class Signup extends React.Component {
+  render () {
+    return (<p>Signup component here</p>);
+  }
+}
+
+Meteor.startup(() => {
+  ReactDOM.render(<Signup/>, document.getElementById('app'));
+});
+
+
+
