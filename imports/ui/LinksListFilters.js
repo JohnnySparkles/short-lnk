044e2343f9e1fe0ce1e14c533363b6073aa6413c
John Wieland <johnny.c.sparkles@gmail.com>
2018-09-22 10:45:49 +1000

    S8.L87: Updates the show hidden links checkbox to use the state and be a controlled component

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { Session } from 'meteor/session';
 
-export default () => {
-  return (
+export default class LinksListFilters extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showVisible: true,
+    }
+  }
+
+  componentDidMount() {
+    this.visibleTracker = Tracker.autorun(() => {
+      const showVisible = Session.get('showVisible');
+
+      this.setState({showVisible: showVisible});
+    });
+  }
+
+  componentWillUnmount() {
+    this.visibleTracker.stop();
+  }
+
+  render() {
+    return (
         <div>
           <label>
-            <input id="id_visible" type="checkbox" onChange={(e) => {
+            <input id="id_visible" checked={!this.state.showVisible} type="checkbox" onChange={(e) => {
               Session.set("showVisible", !e.target.checked);
             }}/>
             Show Hidden
           </label>
         </div>
     );
+  }
 }
 
