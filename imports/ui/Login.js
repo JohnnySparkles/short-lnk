56c143a85a9341df961d917ef79b08f92f9632e4
John Wieland <johnny.c.sparkles@gmail.com>
2018-09-23 19:48:56 +1000

    Started adding styling to the login view

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -29,17 +29,19 @@ export default class Login extends React.Component {
 
   render () {
     return (
-      <div>
-        <h1>Login to Short Lnk</h1>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Login to Short Lnk</h1>
 
-        {this.state.error !== '' ? <p>{this.state.error}</p> : undefined}
+          {this.state.error !== '' ? <p>{this.state.error}</p> : undefined}
 
-        <form onSubmit={this.onSubmit.bind(this)} noValidate>
-          <input type="email" ref="email" name="email" id="email"/>
-          <input type="password" ref="password" name={"password"} id={"password"}/>
-          <button>Login</button>
-        </form>
-        <Link to="/signup">Don't have an account?</Link>
+          <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input type="email" ref="email" name="email" id="email"/>
+            <input type="password" ref="password" name={"password"} id={"password"}/>
+            <button>Login</button>
+          </form>
+          <Link to="/signup">Don't have an account?</Link>
+        </div>
       </div>
     );
   }
